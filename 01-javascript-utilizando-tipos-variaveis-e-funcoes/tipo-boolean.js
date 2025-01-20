0ce33bba5c8406b32928a7c86cacea29e6da7132
Alberto Lima <alberto.btlima@outlook.com>
2025-01-20 16:18:45 -0300

    revisando

---
+++ 01-javascript-utilizando-tipos-variaveis-e-funcoes/tipo-boolean.js
@@ -0,0 +1,14 @@
+const estudante = "Fernando";
+const estaAprovado = false;
+
+if (estaAprovado === true) {
+  console.log("Parabéns, está aprovado!");
+} else {
+  console.log("Infelizmente não esta aprovado");
+}
+
+if (estudante === "Fernando") {
+  console.log(`Olá, ${estudante}`);
+} else {
+  console.log("Quem é você?");
+}
---
+++ 01-javascript-utilizando-tipos-variaveis-e-funcoes/tipo-null-undefined.js
@@ -0,0 +1,15 @@
+let nomeEstudante;
+const professora = "Ana";
+
+console.log(nomeEstudante);
+console.log(typeof nomeEstudante);
+
+let telefone = null;
+
+console.log(telefone);
+console.log(telefone + 2);
+console.log(nomeEstudante + 3);
+
+console.log(typeof nomeEstudante);
+console.log(typeof professora);
+console.log(typeof telefone);
