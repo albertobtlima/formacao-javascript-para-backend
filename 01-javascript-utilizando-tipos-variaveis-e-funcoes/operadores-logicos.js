19159af4247cef0cf4f53af1b60002b851595c36
Alberto Lima <alberto.btlima@outlook.com>
2025-01-22 12:05:29 -0300

    revisando

--- 01-javascript-utilizando-tipos-variaveis-e-funcoes/operadores-logicos.js
+++ 01-javascript-utilizando-tipos-variaveis-e-funcoes/operadores-logicos.js
@@ -1,8 +1,15 @@
 const notaFinal = 7;
-const faltas = 3;
+const faltas = 6;
+const advertencias = 0;
 
 if (notaFinal < 7 || faltas > 4) {
   console.log("Reprovado");
 } else {
   console.log("Não foi reprovado por faltas");
+}
+
+if (faltas <= 2 && !advertencias) {
+  console.log("Recebeu bônus");
+} else {
+  console.log("Não recebeu bônus");
 }
