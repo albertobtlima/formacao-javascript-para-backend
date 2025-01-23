dfb74ebe6aaab612257546ebeccc1020b8e1687b
Alberto Lima <alberto.btlima@outlook.com>
2025-01-23 15:39:11 -0300

    desafios

---
+++ 02-javascript-conhecendo-arrays/map-string.js
@@ -0,0 +1,7 @@
+const nomes = ["dona Florinda", "Senhor barriga", "BRUXA do 71"];
+
+const nomesPadronizados = nomes.map((nome) => {
+  return nome.toUpperCase();
+});
+
+console.log(nomesPadronizados);
