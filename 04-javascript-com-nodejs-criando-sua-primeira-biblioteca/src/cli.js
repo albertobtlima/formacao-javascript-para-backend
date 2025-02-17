56bdb214562d397157681eb8ded4059a5252c376
Alberto Lima <alberto.btlima@outlook.com>
2025-02-17 19:20:20 -0300

    add: program

--- 04-javascript-com-nodejs-criando-sua-primeira-biblioteca/src/cli.js
+++ 04-javascript-com-nodejs-criando-sua-primeira-biblioteca/src/cli.js
@@ -1,21 +1,52 @@
 import fs from "fs";
+import path from "path";
 import trataErros from "./erros/funcoesErro.js";
 import { contaPalavras } from "./index.js";
 import { montaSaidaArquivo } from "./helpers.js";
+import { Command } from "commander";
 
-const caminhoArquivo = process.argv;
-const link = caminhoArquivo[2];
-const endereco = caminhoArquivo[3];
+const program = new Command();
 
-fs.readFile(link, "utf-8", (erro, texto) => {
-  try {
-    if (erro) throw erro;
-    const resultado = contaPalavras(texto);
-    criaESalvaArquivo(resultado, endereco);
-  } catch (erro) {
-    trataErros(erro);
-  }
-});
+program
+  .version("0.0.1")
+  .option("-t, --texto <string>", "caminho do texto a ser encadeado")
+  .option(
+    "-d, --destino <string>",
+    "caminho da pasta onde salvar o arquivo de resultados"
+  )
+  .action((options) => {
+    const { texto, destino } = options;
+
+    if (!texto || !destino) {
+      console.error("Erro: favor inserir caminho de origem e destino");
+      program.help();
+      return;
+    }
+
+    const caminhoTexto = path.resolve(texto);
+    const caminhoDestino = path.resolve(destino);
+
+    try {
+      processaArquivo(caminhoTexto, caminhoDestino);
+      console.log("Texto processado com sucesso");
+    } catch (error) {
+      console.log("Ocorreu um erro", error);
+    }
+  });
+
+program.parse();
+
+function processaArquivo(texto, destino) {
+  fs.readFile(texto, "utf-8", (erro, texto) => {
+    try {
+      if (erro) throw erro;
+      const resultado = contaPalavras(texto);
+      criaESalvaArquivo(resultado, destino);
+    } catch (erro) {
+      trataErros(erro);
+    }
+  });
+}
 
 async function criaESalvaArquivo(listaPalavras, endereco) {
   const arquivoNovo = `${endereco}/resultado.txt`;
